a9a8d8ce21a63ca9d8d369841a8c2a99333143ce
mandyrf <1107060879@qq.com>
2021-01-21 22:26:52 +0800

    Rename MyStack.java to 1.1 设计一个有getMin功能的栈.java

--- 1.1 设计一个有getMin功能的栈.java
+++ 1.1 设计一个有getMin功能的栈.java
@@ -3,21 +3,21 @@ package stackForGetMin;
 import java.util.Stack;
 
 /*
-* ���һ����getMin���ܵ�ջ��
-* һ��ջ�ڻ������ܵĻ����ϣ� ��ʵ�ַ���ջ����СԪ�صĲ���
-* Ҫ��
-* 1. pop, push, getMin ������ʱ�临�Ӷȶ���O(1)
-* 2. ��Ƶ�ջ���Ϳ���ʹ���ֳɵ�ջ�ṹ
+* 设计一个有getMin功能的栈：
+* 一个栈在基本功能的基础上， 再实现返回栈中最小元素的操作
+* 要求：
+* 1. pop, push, getMin 操作的时间复杂度都是O(1)
+* 2. 设计的栈类型可以使用现成的栈结构
 */
 
 
 /*
-* �������ʹ������ջ��һ���������浱ǰջ�е�Ԫ�أ� �书�ܺ���ͨջû������
-* ����stackData�� ��һ��ջ���ڱ���ÿһ������Сֵ������ stackMin
+* 在设计上使用两个栈：一个用来保存当前栈中的元素， 其功能和普通栈没有区别
+* 记作stackData； 另一个栈用于保存每一步的最小值，记作 stackMin
 */
 public class MyStack {
-	// stack.peek() ����ջ��Ԫ�أ� �����ڶ�ջ��ɾ����
-	// stack.pop() ����ջ��Ԫ�أ� ���ڶ�ջ��ɾ����
+	// stack.peek() 返回栈顶元素， 但不在堆栈中删除它
+	// stack.pop() 返回栈顶元素， 并在堆栈中删除它
 	
     private Stack<Integer> stackData;
     private Stack<Integer> stackMin;
